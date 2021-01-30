574602872e9e2fc73ba0b9c06db6ec61be30e06f
Stephen Reid <stephen@stephenreid.net>
2021-01-30 11:16:27 +0000

    .

--- models/account.rb
+++ models/account.rb
@@ -52,7 +52,7 @@ class Account
     all_members.each do |member|
       name = member.name
       email = member.profile.email
-      next if !email || member.is_invited_user
+      next if !email || member.is_invited_user || member.deleted
 
       puts "#{name} #{email}"
       account = Account.find_by(email: email.downcase) || Account.create(name: name, email: email)
