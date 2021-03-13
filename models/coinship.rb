804817c47122ac7396850580e2229ca07ca2136f
Stephen Reid <stephen@stephenreid.net>
2021-03-13 23:27:23 +0000

    .

--- models/coinship.rb
+++ models/coinship.rb
@@ -44,8 +44,8 @@ class Coinship
       holding_before = (units_before || 0) + (units_elsewhere_sum_before || 0)
       holding_after = (units_after || 0) + (units_elsewhere_sum_after || 0)
       holding_change = holding_after - holding_before
-      if holding_change.abs > 1e-6
-        holding_percentage_change = (100 * (holding_after - holding_before) / holding_before).round(1)
+      holding_percentage_change = (100 * holding_change / holding_before).round(1)
+      if holding_percentage_change.abs > 1
         message = if holding_percentage_change.infinite?
                     "<@#{account.slack_id}> bought a fresh batch of <https://www.coingecko.com/en/coins/#{coin.slug}|#{coin.symbol}> https://autopia.co/u/#{account.username}"
                   else
